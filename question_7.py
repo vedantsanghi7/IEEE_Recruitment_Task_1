4b88a1db4a7e82d981c982b8f742d2067cfe6406
Vedant Sanghi <f20250652@pilani.bits-pilani.ac.in>
2025-08-30 14:36:52 +0530

    Added Q7 file

---
+++ question_7.py
@@ -0,0 +1,13 @@
+list1 = [3, 4, 5, 1, 4, 6, 1, 7, 7]
+list2 = [5, 8, 2, 9, 9, 4, 6, 3]
+
+set1 = set(list1)
+set2 = set(list2)
+intersection_set = set1 & set2
+
+final_list = list(intersection_set)
+
+print(f"List 1: {list1}")
+print(f"List 2: {list2}")
+print(f"\nIntersection of the two lists: {final_list}")
+
